92985f4c71f6061e0493b0841cfcaa32ee913376
Lucas-Hudson <lucashudson.contact@gmail.com>
2019-07-09 11:50:30 +0200

    exo 8-10

---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Quel est ton prénom ?"
+firstName = gets.chomp
+puts "Quel est ton nom ?"
+lastName = gets.chomp
+puts "Bonjour #{firstName} #{lastName}"
---
+++ exo_10.rb
@@ -0,0 +1,5 @@
+annee = 2017
+
+puts "Quel est ton annee de naissance ?"
+num = gets.chomp.to_i
+puts "En 2017 tu avais #{annee - num}."
