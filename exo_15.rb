701364a599bba72aff19e60b032cf0d838818c5b
Syle <sylvain.firer@gmail.com>
2019-07-10 10:24:02 +0200

    fix 15

--- exo_15.rb
+++ exo_15.rb
@@ -4,6 +4,6 @@ annee_naissance = gets.chomp.to_i
 num2 = num - annee_naissance
 num2.times do |i|
     print annee_naissance + i + 1
-    puts "ton age est #{i + 1} "
+    puts " ton age est de #{i + 1} ans "
 end
 
